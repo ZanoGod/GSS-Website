3c404cfe4592b97569091c06ac5aae805c539bbb
ZanoGod <zinwaiyan1443@gmail.com>
2026-04-07 14:02:24 +0630

    fix qr

--- js/fleet.js
+++ js/fleet.js
@@ -17,7 +17,7 @@ const cars = [
     slug: 'mitsubishi-asx',
     name: 'Mitsubishi ASX',
     category: 'suv',
-    image: '../img/cars/Mitsubishi ASX .webp',
+    image: '../img/cars/Mitsubishi ASX .png',
     passengers: 4,
     transmission: 'Automatic',
     fuel: 'Gasoline',
@@ -43,7 +43,7 @@ const cars = [
     slug: 'toyota-hiace',
     name: 'Toyota Hiace',
     category: 'mini-van',
-    image: '../img/cars/toyota_hiace.webp',
+    image: '../img/cars/toyota_hiace.png',
     passengers: 14,
     transmission: 'Automatic',
     fuel: 'Gasoline',
@@ -338,7 +338,7 @@ function displayCars(category) {
                 <i class="fa fa-car text-dark"></i><span class="text-body ms-1">${car.type}</span>
               </div>
             </div>
-
+            <a href="#" class="btn btn-primary rounded-pill d-flex justify-content-center py-3">Book Now</a>
           </div>
         </div>
       </div>
